13e833e2cda4d346dfd557752ab5bf3cf004656f
Giovanna Galhardo Julio <gigalhardo98@gmail.com>
2022-07-25 16:02:27 -0300

    Forms funcionais e requisições prontas

--- hooks-labe-food1/src/pages/Profile/EditProfilePage.js
+++ hooks-labe-food1/src/pages/Profile/EditProfilePage.js
@@ -1,53 +1,109 @@
-import React from "react";
-import Header from "../../components/Header/Header";
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import Header from '../../components/Header/Header';
+import { BASE_URL } from '../../constants/BASE_URL';
+import useForm from '../../hooks/useForm';
+import { useProtectedPage } from '../../hooks/useProtectedPage';
+import useRequestData from '../../hooks/useRequestData';
 import {
-  StyledButton,
-  StyledDiv,
-  StyledDivInput,
-  DivInput,
-  StyledInput,
-} from "../../Styled";
+	StyledButton,
+	StyledDiv,
+	StyledDivInput,
+	DivInput,
+	StyledInput,
+} from '../../Styled';
 
 const EditProfilePage = () => {
-  return (
-    <StyledDiv>
-      <Header name='Editar' />
-      <form>
-        <StyledDivInput>
-          <DivInput>
-            <StyledInput
-              label={"Nome"}
-              variant="outlined"
-              placeholder="Bruna Oliveira"
-              required
-            />
-          </DivInput>
-          <DivInput>
-            <StyledInput
-              label={"E-mail"}
-              variant="outlined"
-              placeholder="bruna_o@gmail.com"
-              required
-            />
-          </DivInput>
-          <DivInput>
-            <StyledInput
-              label={"CPF"}
-              variant="outlined"
-              placeholder="333.333.333-33"
-              pattern="\d{3}\.\d{3}\.\d{3}-\d{2}"
-              title="Digite um CPF no formato: xxx.xxx.xxx-xx"
-              required
-            />
-          </DivInput>
+	useProtectedPage();
 
-          <StyledButton color="primary" variant="contained">
-            Salvar
-          </StyledButton>
-        </StyledDivInput>
-      </form>
-    </StyledDiv>
-  );
+	const currentProfileInfo = useRequestData({}, `${BASE_URL}/profile`);
+
+	const [form, setForm, handleInputChange] = useForm({
+		name: '',
+		email: '',
+		cpf: '',
+	});
+
+	// useEffect(() => {
+	// 	if (currentProfileInfo) {
+	// 		setForm({
+	// 			name: currentProfileInfo.data?.user?.name,
+	// 			email: currentProfileInfo.data?.user?.email,
+	// 			cpf: currentProfileInfo.data?.user?.cpf,
+	// 		});
+	// 	}
+	// }, [currentProfileInfo]);
+
+	const onSubmitProfileEdit = (event) => {
+		event.preventDefault();
+
+		const url = `${BASE_URL}/profile`;
+
+		const headers = {
+			headers: {
+				auth: localStorage.getItem('token'),
+			},
+		};
+
+		axios
+			.put(url, form, headers)
+			.then((response) => {
+				localStorage.setItem('token', response.data.token);
+				alert('Alterado com sucesso.');
+			})
+			.catch((error) => {
+				alert(error.response);
+			});
+	};
+
+	return (
+		<StyledDiv>
+			<Header name='Editar' />
+			<form onSubmit={onSubmitProfileEdit}>
+				<StyledDivInput>
+					<DivInput>
+						<StyledInput
+							label={'Nome'}
+							variant='outlined'
+							placeholder='Bruna Oliveira'
+							required
+							name='name'
+							value={form.name}
+							onChange={handleInputChange}
+						/>
+					</DivInput>
+					<DivInput>
+						<StyledInput
+							label={'E-mail'}
+							variant='outlined'
+							placeholder='bruna_o@gmail.com'
+							required
+							name='email'
+							value={form.email}
+							onChange={handleInputChange}
+						/>
+					</DivInput>
+					<DivInput>
+						<StyledInput
+							label={'CPF'}
+							variant='outlined'
+							placeholder='333.333.333-33'
+							pattern='\d{3}\.\d{3}\.\d{3}-\d{2}'
+							title='Digite um CPF no formato: xxx.xxx.xxx-xx'
+							required
+							name='cpf'
+							value={form.cpf}
+							onChange={handleInputChange}
+						/>
+					</DivInput>
+
+					<StyledButton color='primary' variant='contained' type='submit'>
+						Salvar
+					</StyledButton>
+				</StyledDivInput>
+			</form>
+		</StyledDiv>
+	);
 };
 
 export default EditProfilePage;
